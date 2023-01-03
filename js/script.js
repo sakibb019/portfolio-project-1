21c58078519066d5a9ec9c621ee0f32259572eff
Mohammad Sakib Mahmood <sakibmahmood485@gmail.com>
2023-01-03 14:16:56 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ======= typing animation ======= */
+var typed = new Typed(".typing", {
+    strings: ["Web Developer", "Freelancer", "Designer", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+
+})
